af3fe33de16d47d88870d767c91a70a3a94c5537
yunusemreyakisan <yunusemreyakisan@outlook.com>
2022-02-10 16:13:49 +0300

    Day 1

--- degiskenler.dart
+++ degiskenler.dart
@@ -8,3 +8,53 @@ void main() {
 
   print("Benim adım " + name + " " + surname + " ve yaşım " + age + "'dir.");
 }
+
+//String ifade içerisine $ işareti koyularak çıktıya değişken eklenebilir.
+void method() {
+  var ad = "Yunus Emre";
+  int yas = 21;
+  print("$ad Bursa'da $yas yıldır yaşıyor.");
+}
+
+//Örnek-1
+void degiskenOrnek1() {
+  int a = 20;
+  var b = 40;
+  print("$a ve $b'nin toplamı : ${a + b}'dir.");
+}
+
+//Değişken Oluşturma Çeşitleri
+var sayi = 10;
+int numara = 61;
+
+//Örnek 2 : Değişkenlerin değerini sonradan değiştirebilirsiniz.
+void degiskenler() {
+  var toplam = null;
+  var bosDegisken;
+  var fiyat = 99.99;
+  double ucret = 25.99;
+  print(fiyat); //99.99
+  print(ucret); //25.99
+
+  fiyat = 100;
+  ucret = 88;
+  print(fiyat); //100
+  print(ucret); //88
+
+  toplam = fiyat +
+      ucret; //İşlem yapıldıktan sonra başka bir değişkene veri aktarılabilir.
+  print(toplam);
+
+  bosDegisken =
+      "Bursa"; //Boş değişken oluşturup daha sonra değer aktarabiliriz.
+}
+
+//Ornek 3 : Yan yana değişken oluşturup yazdırabiliriz.
+void degiskenler1() {
+  var s1, s2, s3;
+  s1 = 20;
+  s2 = 10;
+  s3 = 30;
+
+  print("$s1, $s2, $s3");
+}
