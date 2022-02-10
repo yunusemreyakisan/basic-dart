23cfb792cf23f47e9858028e5a04fb0eb58a5a46
yunusemreyakisan <yunusemreyakisan@outlook.com>
2022-02-10 15:34:04 +0300

    Day 1

--- degiskenler.dart
+++ degiskenler.dart
@@ -1,1 +1,10 @@
+//Programcılıkta kullandığımız dilin izin verdiği veri tipleri çerçevesinde istediğimiz verileri, bilgileri saklamamıza sağlayan yapılara değişken (variable) denilmektedir.
+//Değişkenlere verdiğimiz isimler ile erişebilir, birbirinden ayırabilir, işlemlerimizi yapabilir.
 
+void main() {
+  var name = "Yunus Emre";
+  var surname = "Yakışan";
+  var age = "21";
+
+  print("Benim adım " + name + " " + surname + " ve yaşım " + age + "'dir.");
+}
