c81c0c63d3b73ed2e12f15d23ac3050e872a29c0
yunusemreyakisan <yunusemreyakisan@outlook.com>
2022-02-10 15:20:38 +0300

    Day 1

---
+++ giris.dart
@@ -0,0 +1,10 @@
+// Google	tarafından	geliştirilmiştir.
+// Google’ın	sahip	olduğu	Flutter platformu	üzerinde	kullanılmaktadır.
+// Flutter,	tek	seferde	hem	android hemde ios için	mobil	uygulama geliştirilen	bir	platformdur.
+// JavaScript diline çok benzemektedir.
+
+
+// Dart ve Flutter için Yararlı Kaynak Önerileri 
+// https://flutterdersleri.com/kaynaklar.html
+// https://mertcankiyak.com/flutter-ogrenmek-icin-kaynak-onerileri/
+// 
