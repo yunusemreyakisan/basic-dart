7a4448d7e7686d3065c3f37e1334a424c3c91c04
yunusemreyakisan <yunusemreyakisan@outlook.com>
2022-02-11 20:59:14 +0300

    Day 2

--- Dart/donguler.dart
+++ Dart/donguler.dart
@@ -1,26 +1,24 @@
-// // Döngüler	belirli	kodları	tekrarlı	çalıştırma	amaçlı	yapılardır.
+// Döngüler	belirli	kodları	tekrarlı	çalıştırma	amaçlı	yapılardır.
 
-// //for döngüsü : for (var VERİ in VERİ KÜMESİ) {TEKRARLANAN KOD YAPISI}
-// //Örnek-1
-// import 'dart:io';
+//for döngüsü : for (var VERİ in VERİ KÜMESİ) {TEKRARLANAN KOD YAPISI}
+//Örnek-1
+var deger;
+void main() {
+  var dizi = [10, 20, 30];
 
-// var deger;
-// void main() {
-//   var dizi = [10, 20, 30];
+  for (var deger in dizi) {
+    print("Sonuç: $deger");
+  }
+}
 
-//   for (var deger in dizi) {
-//     print("Sonuç: $deger");
-//   }
-// }
-
-// //While döngüsü: while(ŞART){Tekrarlana ifade}
-// var sayac = 1;
-// void ana() {
-//   while (sayac < 4) {
-//     print("Sonuç: $sayac");
-//     sayac += 1;
-//   }
-// }
+//While döngüsü: while(ŞART){Tekrarlana ifade}
+var sayac = 1;
+void ana() {
+  while (sayac < 4) {
+    print("Sonuç: $sayac");
+    sayac += 1;
+  }
+}
 
 //break : İşlemi bitirir.
 //continue : İşlemi devam ettirir.
