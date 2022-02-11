2990d712f23a5b4e95e7b13db4012f2d4f255021
yunusemreyakisan <yunusemreyakisan@outlook.com>
2022-02-11 20:30:14 +0300

    added alistirma

--- Dart/ifyapisi.dart
+++ Dart/ifyapisi.dart
@@ -5,6 +5,8 @@
 //}
 
 //Örnek-1
+import 'dart:io';
+
 void main() {
   var yas = 21;
 
@@ -47,5 +49,48 @@ void elseifyapisi() {
     print("a harfine atanan sayı 6'dan farklıdır.");
   } else {
     print("a harfine atanan sayı bulunamadı.");
+  }
+}
+
+//Kısaltma
+//if (koşul) DOĞRU SONUÇ else YANLIŞ SONUÇ
+//Bu şekilde tek satırda yazabilirsiniz.
+
+void kisaltma() {
+  int a = 5;
+  int b = 10;
+
+  if (a == b)
+    print("a ile b eşittir.");
+  else {
+    print("a ile b eşit değildir.");
+  }
+}
+
+//Alıştırma-1 : Kullanıcıdan alınan seçime göre hesap yapan programı yazınız.
+var dAlan, cAlan, secim;
+var kKenar, uKenar, yariCap;
+var pi = 3.14;
+
+void alanHesabi() {
+  print("Dikdörtgen Alanı (1)\nÇember Alanı (2)\nSeçiminizi yapınız: ");
+  secim = stdin.readLineSync();
+
+  if (secim == 1) {
+    print("Kısa kenar giriniz: ");
+    kKenar = stdin.readLineSync();
+    print("Uzun kenar giriniz: ");
+    uKenar = stdin.readLineSync();
+    dAlan = kKenar * uKenar;
+
+    print("Dikdörtgeninizin alanı: $dAlan");
+  } else if (secim == 2) {
+    print("Yarıçap giriniz: ");
+    yariCap = stdin.readLineSync();
+    cAlan = pi * (yariCap * yariCap);
+    print("Çemberiniz alanı: $cAlan");
+  } else {
+    print("Yanlış tuşlama yaptınız.");
+    return alanHesabi();
   }
 }
