012fda5f3d30393ea0ece2b3889eb315f5df5627
yunusemreyakisan <yunusemreyakisan@outlook.com>
2022-02-11 20:10:36 +0300

    fixed

--- Dart/ifyapisi.dart
+++ Dart/ifyapisi.dart
@@ -1,1 +1,51 @@
+//if koşul yapısı
 
+//if (şart){
+//şarta bağlı kod(true ise)
+//}
+
+//Örnek-1
+void main() {
+  var yas = 21;
+
+  if (yas == 21) {
+    print("Sen 21 yaşındasın.");
+  }
+}
+
+//else
+void elseornek() {
+  var yas = 21;
+
+  if (yas == 21) {
+    print("Sen 21 yaşındasın.");
+  } else {
+    print("Yaşın 21 değil.");
+  }
+}
+
+//Çoklu Şart
+var isim = "Emre";
+var yas = 21;
+
+void fonksiyon() {
+  if (isim == "Emre" && yas == 21) {
+    //Her iki şartında sağlanması gerekiyor.
+    print("Sen 21 yaşındasın ve adın Emre");
+  } else {
+    print("Bilgilerin hiçbiri doğru değil.");
+  }
+}
+
+//else if yapısı
+void elseifyapisi() {
+  var a = 5;
+
+  if (a == 5) {
+    print("a harfine atanan sayı 5'tir.");
+  } else if (a != 6) {
+    print("a harfine atanan sayı 6'dan farklıdır.");
+  } else {
+    print("a harfine atanan sayı bulunamadı.");
+  }
+}
