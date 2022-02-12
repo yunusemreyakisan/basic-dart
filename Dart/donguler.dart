7e20a4e42f0847bf0da0c358c8b15a8b9121d71b
yunusemreyakisan <yunusemreyakisan@outlook.com>
2022-02-12 18:57:02 +0300

    Day 3

--- Dart/donguler.dart
+++ Dart/donguler.dart
@@ -2,6 +2,8 @@
 
 //for döngüsü : for (var VERİ in VERİ KÜMESİ) {TEKRARLANAN KOD YAPISI}
 //Örnek-1
+import 'dart:math';
+
 var deger;
 void main() {
   var dizi = [10, 20, 30];
@@ -30,4 +32,28 @@ void tekrar() {
   for (i = 0; i < 5; i++) {
     print("Konsoldan girilen isim: $isim");
   }
+}
+
+//Bazı Hazır Komutlar
+int min = 5;
+int max = 10;
+void hazir() {
+  var r = Random();
+
+  int rastegeleSayi = min + r.nextInt((max - min) + 1); //5-10 arası
+}
+
+//Matematiksel İfadeler
+
+void mathifade() {
+  double x = 6.5;
+
+  int c = x.ceil();
+  print("c: $c");
+
+  int f = x.floor();
+  print("f: $f");
+
+  double s = sqrt(x);
+  print("S: $s");
 }
