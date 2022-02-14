7e9b0a3f04bdc72526eba48589a30924f7dc77c7
yunusemreyakisan <yunusemreyakisan@outlook.com>
2022-02-14 15:55:10 +0300

    Day 5

--- Dart/Nesne Tabanli Programlama/fonksiyonlar.dart
+++ Dart/Nesne Tabanli Programlama/fonksiyonlar.dart
@@ -66,3 +66,38 @@ print(gelendonusluSelam) {
   // TODO: implement print
   throw UnimplementedError();
 }
+
+//Fonksiyonların parametre alması
+// Parametre	fonksiyonlara	dışarıdan	verilen	değerlerdir.
+// Her	fonksiyonun	parametresi	olmak	zorunda	değildir.
+// Parametreler	tanımlaması	değişkeni	tanımlar	gibidir.
+// Parametreler	,	virgül	ile	birden	fazla	tanımlanabilir.
+
+//Ornek-1
+void selamlar(String isim) {
+  String sonuc = "Merhaba $isim";
+  print(sonuc);
+
+  selamlar("Emre");
+}
+
+//Birden fazla parametre de kullanılabilir.
+
+int hesaplama(int sayi1, int sayi2) {
+  int toplama = sayi1 + sayi2;
+  return toplama;
+}
+
+int toplam = hesaplama(20, 20);
+
+//Ödev-1 : Kenarları	parametre	olarak	girilen	ve	dikdörtgenin	çevresini	hesaplayan	bir	metod yazınız.
+
+hesapla(int kKenar, int uKenar) {
+  int kenarHesabi = kKenar * uKenar;
+  return kenarHesabi;
+}
+
+void anaCalistir() {
+  int dikdortgenHesapla = hesapla(2, 5);
+  print(dikdortgenHesapla);
+}
